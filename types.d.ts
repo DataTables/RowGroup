4c969fcdadff8b502aedabccd73c0d6be1e53738
Allan Jardine <allan@datatables.net>
2022-10-24 17:22:53 +0000

    New: Typescript definitions rewritten to fully interoperate with DataTables own Typescript definitions.

--- types.d.ts
+++ types.d.ts
@@ -1,105 +1,145 @@
-// Type definitions for datatables.net-rowgroup 1.0
+// Type definitions for DataTables RowGroup
+//
 // Project: https://datatables.net/extensions/rowgroup/, https://datatables.net
-// Definitions by: Matthieu Tabuteau <https://github.com/maixiu>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
+// Definitions by:
+//   SpryMedia
+//   Matthieu Tabuteau <https://github.com/maixiu>
 
 /// <reference types="jquery" />
-/// <reference types="datatables.net"/>
 
-declare namespace DataTables {
-    interface Settings {
-        /**
-         * RowGroup extension options
-         */
-        rowGroup?: boolean | RowGroupSettings;
-    }
+import DataTables, {Api} from 'datatables.net';
 
-    interface StaticFunctions {
-        RowGroup: RowGroupStaticFunctions;
-    }
+export default DataTables;
 
-    interface RowGroupStaticFunctions {
-        new (dt: Api<any>, settings: boolean | RowGroupSettings): undefined;
-        version: string;
-        defaults: RowGroupSettings;
-    }
 
-    interface Api<T> {
-        rowGroup(): RowGroupApi;
-    }
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * DataTables' types integration
+ */
+declare module 'datatables.net' {
+	interface Config {
+		/**
+		 * RowGroup extension options
+		 */
+		rowGroup?: boolean | ConfigRowGroup;
+	}
 
-    interface RowGroupApi {
-        /**
-         * Get the data source for the row grouping
-         * 
-         * @returns Data source property
-         */
-        dataSrc(): number | string;
+	interface Api<T> {
+		/**
+		 * RowGroup methods container
+		 * 
+		 * @returns Api for chaining with the additional RowGroup methods
+		 */
+		rowGroup(): ApiRowGroup<T>;
+	}
 
-        /**
-         * Set the data source for the row grouping
-         * 
-         * @param prop Data source property
-         * @returns DataTables Api instance
-         */
-        dataSrc(prop: number|string): Api<any>;
+	interface ApiStatic {
+		/**
+		 * RowGroup class
+		 */
+		RowGroup: {
+			/**
+			 * Create a new RowGroup instance for the target DataTable
+			 */
+			new (dt: Api<any>, settings: boolean | ConfigRowGroup);
 
-        /**
-         * Disable RowGroup's interaction with the table
-         * 
-         * @returns DataTables API instance
-         */
-        disable(): Api<any>;
+			/**
+			 * Default configuration values
+			 */
+			defaults: ConfigRowGroup;
 
-        /**
-         * Enable or disable RowGroup's interaction with the table
-         * 
-         * @param enable Either enables or disables RowGroup depending on the value of the flag
-         * @returns DataTables Api instance
-         */
-        enable(enable?: boolean): Api<any>;
+			/**
+			 * RowGroup version
+			 */
+			version: string;
+		}
+	}
+}
 
-        /**
-         * Get the enabled state for RowGroup.
-         * 
-         * @returns true if enabled, false otherwise
-         */
-        enabled(): boolean;
-    }
 
-    /**
-     * RowGroup extension options
-     */
-    interface RowGroupSettings {
-        /**
-         * Set the class name to be used for the grouping rows
-         */
-        className?: string;
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Options
+ */
 
-        /**
-         * Set the data point to use as the grouping data source
-         */
-        dataSrc?: number|string;
+interface ConfigRowGroup {
+	/**
+	 * Set the class name to be used for the grouping rows
+	 */
+	className?: string;
 
-        /**
-         * Provides the ability to disable row grouping at initialisation
-         */
-        enable?: boolean;
+	/**
+	 * Set the data point to use as the grouping data source
+	 */
+	dataSrc?: number|string;
 
-        /**
-         * Set the class name to be used for the grouping end rows
-         */
-        endClassName?: string;
+	/**
+	 * Text to show for rows which have `null`, `undefined` or empty string group data
+	 * 
+	 * @since 1.0.2
+	 */
+	 emptyDataGroup?: string;
 
-        /**
-         * Provide a function that can be used to control the data shown in the end grouping row
-         */
-        endRender?: (rows: Api<any>, group: string) => string|HTMLElement|JQuery;
+	/**
+	 * Provides the ability to disable row grouping at initialisation
+	 */
+	enable?: boolean;
 
-        /**
-         * Provide a function that can be used to control the data shown in the start grouping row
-         */
-        startRender?: (rows: Api<any>, group: string) => string|HTMLElement|JQuery;
-    }
+	/**
+	 * Set the class name to be used for the grouping end rows
+	 */
+	endClassName?: string;
+
+	/**
+	 * Provide a function that can be used to control the data shown in the end grouping row
+	 */
+	endRender?: (rows: Api<any>, group: string) => string|HTMLElement|JQuery;
+
+	/**
+	 * Set the class name to be used for the start grouping rows
+	 */
+	startClassName?: string;
+
+	/**
+	 * Provide a function that can be used to control the data shown in the start grouping row
+	 */
+	startRender?: (rows: Api<any>, group: string) => string|HTMLElement|JQuery;
+}
+
+
+interface ApiRowGroup<T> extends Api<T> {
+	/**
+	 * Get the data source for the row grouping
+	 * 
+	 * @returns Data source property
+	 */
+	dataSrc(): number | string;
+
+	/**
+	 * Set the data source for the row grouping
+	 * 
+	 * @param prop Data source property
+	 * @returns DataTables Api instance
+	 */
+	dataSrc(prop: number|string): Api<T>;
+
+	/**
+	 * Disable RowGroup's interaction with the table
+	 * 
+	 * @returns DataTables API instance
+	 */
+	disable(): Api<T>;
+
+	/**
+	 * Enable or disable RowGroup's interaction with the table
+	 * 
+	 * @param enable Either enables or disables RowGroup depending on the value of the flag
+	 * @returns DataTables Api instance
+	 */
+	enable(enable?: boolean): Api<T>;
+
+	/**
+	 * Get the enabled state for RowGroup.
+	 * 
+	 * @returns true if enabled, false otherwise
+	 */
+	enabled(): boolean;
 }
