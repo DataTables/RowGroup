e1cb59be23180030d3410fa81841f03eb42b0dbb
Allan Jardine <allan@datatables.net>
2023-07-04 14:25:36 +0100

    Release 1.4.0

--- js/dataTables.rowGroup.js
+++ js/dataTables.rowGroup.js
@@ -1,11 +1,11 @@
-/*! RowGroup 1.4.0-dev
+/*! RowGroup 1.4.0
  * © SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     RowGroup
  * @description RowGrouping for DataTables
- * @version     1.4.0-dev
+ * @version     1.4.0
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     datatables.net
  * @copyright   SpryMedia Ltd.
@@ -369,7 +369,7 @@ RowGroup.defaults = {
 	}
 };
 
-RowGroup.version = '1.4.0-dev';
+RowGroup.version = '1.4.0';
 
 $.fn.dataTable.RowGroup = RowGroup;
 $.fn.DataTable.RowGroup = RowGroup;
