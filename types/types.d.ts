c1d568b64ec98d2986ff3df442a277217232a091
Allan Jardine <allan@datatables.net>
2024-02-14 11:56:06 +0000

    Types: new constructor returns instance, not void

--- types/types.d.ts
+++ types/types.d.ts
@@ -40,7 +40,7 @@ declare module 'datatables.net' {
 			/**
 			 * Create a new RowGroup instance for the target DataTable
 			 */
-			new (dt: Api<any>, settings: boolean | ConfigRowGroup): void;
+			new (dt: Api<any>, settings: boolean | ConfigRowGroup): DataTablesStatic['RowGroup'];
 
 			/**
 			 * Default configuration values
