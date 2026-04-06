1d80efc25e05b80d202aa5699e2950417ecaf4ef
Allan Jardine <allan@datatables.net>
2026-04-06 15:38:27 +0000

    Release 2.0.0-beta.1

--- src/RowGroup.ts
+++ src/RowGroup.ts
@@ -59,7 +59,7 @@ export default class RowGroup {
 		}
 	};
 
-	static version = '1.6.0';
+	static version = '2.0.0-beta.1';
 
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	 * API methods for DataTables API interface
